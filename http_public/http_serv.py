bc6f1ab1f2478cfdb1215145540a8c1f191987f7
Kaj Bostrom <bostromk@cs.washington.edu>
2016-12-04 15:27:04 -0800

    server stuff

--- http_public/http_serv.py
+++
@@ -1,7 +0,0 @@
-from http.server import *
-import shutil
-
-def run():
-  server_address = ('', 8000)
-  httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
-  httpd.serve_forever()
---
+++ http_serv.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3
+
+from http.server import *
+import os
+
+os.chdir('http_public/')
+server_address = ('', 8000)
+httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+httpd.serve_forever()
