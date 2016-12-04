24c46e361c829eeab437ec03d53363ba532a07fb
Kaj Bostrom <bostromk@cs.washington.edu>
2016-12-04 15:17:17 -0800

    fill cornfield.js

--- http_public/cornfield.js
+++ http_public/cornfield.js
@@ -1,8 +1,31 @@
 
 "use strict";
 
-window.addEventListener('load',
-  function(e) {
-    ws = WebSocket()
+window.addEventListener('load', function(e) {
+  ws = new WebSocket('ws://'+window.location.hostname+':8765/');
+  ws.addEventListener('open', function(e) {
+    //ws.send("foo");
+  });
+  ws.addEventListener('message', function(e) {
+    logEntry = document.createElement('div');
+    logEntry.appendChild(document.createTextNode(e.data));
+    document.getElementById('log').appendChild(logEntry);
+  });
+  function submit() {
+    prompt = document.getElementById('prompt');
+    ws.send(prompt.value);
+    prompt.value = '';
   }
-);
+  document.getElementById('prompt').addEventListener('keydown', function(e) {
+    if (e.key) {
+      if (e.key == '\n') {
+        submit();
+      }
+    } else if (e.which) {
+      if (e.which == 13) {
+        submit();
+      }
+    }
+  });
+  document.getElementById('prompt').focus();
+});
