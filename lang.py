8aa765d5735992a68a2f1a2ef85e78e7566a7948
Kaj Bostrom <bostromk@cs.washington.edu>
2017-02-03 11:38:20 -0800

    fix left application

--- lang.py
+++ lang.py
@@ -3,9 +3,9 @@ from vocab import *
 
 def lam_apply(l_edgess, r_edgess, fringe_entry):
   (l_i, r_i, item) = fringe_entry
-  if l_i-1 > 0:
+  if l_i-1 >= 0:
     for (far_l_i, l_item) in r_edgess[l_i-1]:
-      if l_item.bilam and far_l_i-1 > 0:
+      if l_item.bilam and far_l_i-1 >= 0:
         for (further_l_i, far_l_item) in r_edgess[far_l_i-1]:
           res_bi_l = l_item.bilam(l_item, far_l_item, item)
           if res_bi_l:
@@ -29,7 +29,7 @@ def lam_apply(l_edgess, r_edgess, fringe_entry):
       res = item.rlam(item, r_item)
       if res:
         yield (l_i, far_r_i, res)
-    if item.bilam and l_i-1 > 0:
+    if item.bilam and l_i-1 >= 0:
       for (far_l_i, l_item) in r_edgess[l_i-1]:
         for (far_r_i, r_item) in l_edgess[r_i+1]:
           res_bi = item.bilam(item, l_item, r_item)
@@ -39,7 +39,7 @@ def lam_apply(l_edgess, r_edgess, fringe_entry):
 def parse_tokens(tokenss, verbose=False):
   l_edgess = [[] for i in range(len(tokenss))]
   r_edgess = [[] for i in range(len(tokenss))]
-  fringe = [(i, i, t) for i in range(len(tokenss)) for t in tokenss[i]]
+  fringe = [(i, i, t) for i in reversed(range(len(tokenss))) for t in tokenss[i]]
   while len(fringe) > 0:
     (l_i, r_i, item) = fringe_entry = fringe.pop()
     print("Popped", fringe_entry)
@@ -49,6 +49,7 @@ def parse_tokens(tokenss, verbose=False):
     l_edgess[l_i].append((r_i, item))
     r_edgess[r_i].append((l_i, item))
     for new_entry in lam_apply(l_edgess, r_edgess, fringe_entry):
+      print("Pushing", new_entry)
       (new_l_i, new_r_i, new_item) = new_entry
       fringe.append(new_entry)
 
