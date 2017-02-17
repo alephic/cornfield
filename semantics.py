0d9d1e900bd33f7436c819b0143b9353e4f99b00
Kaj Bostrom <bostromk@cs.washington.edu>
2017-02-17 14:16:07 -0800

    work on referents

--- semantics.py
+++ semantics.py
@@ -39,15 +39,56 @@ def collect_ref_nodes(tree):
   return tree_collect(tree,
     lambda n: n.reln in nominal_relns or (n.reln == 'root' and n.tag in nominal_tags))
 
+ANY = object()
+
+def matches(pat, ex):
+  if ex == ANY or pat == ANY:
+    return True
+  else return pat == ex
+
+class InterlocRef:
+  intrn = {}
+  def __init__(self, pers, plur):
+    self.pers = pers
+    self.plur = plur
+def get_interloc_ref(pers, plur=False):
+  if (pers, plur) in InterlocRef.intrn:
+    return InterlocRef.intrn[(pers, plur)]
+  else:
+    ref = InterlocRef(pers, plur)
+    InterlocRef.intrn[(pers, plur)] = ref
+    return ref
+
+class FixedRef:
+  intrn = {}
+  def __init__(self, lex):
+    self.lex = lex
+def get_fixed_ref(lex):
+  if lex in FixedRef.intrn:
+    return FixedRef.intrn[lex]
+  else:
+    ref = FixedRef(lex)
+    FixedRef.intrn[lex] = ref
+    return ref
+
 class World:
   def __init__(self, referents, relations):
     self.referents = referents
     self.relations = relations
     self.mentions = deque()
-  def process(stmt):
+  def process(stmt, speaker_id):
     for ref_node in collect_ref_nodes(stmt):
       ref = self.get_ref_for(ref_node)
+      self.mentions.appendleft(ref)
   def get_ref_for(node):
+    if (node.tag == 'NN' and 'det' not in node.children) or node.tag == 'NNP':
+      return FixedRef(node.stem)
+    if node.tag == 'PRP':
+      if node.stem == 'I' or node.stem == 'myself':
+        return get_interloc_ref(1)
+      if node.stem == 'we' or node.stem == 'ourselves':
+        return get_interloc_ref(1, plur=True)
+      if node.stem == 'you' or node.stem == 'yourself':
     # Indefinites become templates
     # Definites become templates restricted to existing referents
     # Pronouns are dereferenced to their last-mention buddy
