3dcbaff0b744fd0cdb7627d4d475a3081c86a16e
Kaj Bostrom <bostromk@cs.washington.edu>
2017-02-17 16:34:38 -0800

    work on get_ref_for

--- semantics.py
+++ semantics.py
@@ -41,16 +41,20 @@ def collect_ref_nodes(tree):
 
 ANY = object()
 
-def matches(pat, ex):
-  if ex == ANY or pat == ANY:
+def matches(x1, x2):
+  if x1 == ANY or x2 == ANY:
     return True
-  else return pat == ex
+  else return x1 == x2
 
 class InterlocRef:
   intrn = {}
   def __init__(self, pers, plur):
     self.pers = pers
     self.plur = plur
+  def __eq__(self, other):
+    return isinstance(other, InterlocRef) and matches(other.pers, self.pers) and matches(other.plur, self.plur)
+  def __hash__(self, other):
+    return hash(InterlocRef) ^ hash(self.pers) ^ hash(self.plur)
 def get_interloc_ref(pers, plur=False):
   if (pers, plur) in InterlocRef.intrn:
     return InterlocRef.intrn[(pers, plur)]
@@ -81,14 +85,30 @@ class World:
       ref = self.get_ref_for(ref_node)
       self.mentions.appendleft(ref)
   def get_ref_for(node):
+    if node.tag == 'PRP':
+      if node.stem in ('I', 'me', 'myself'):
+        return get_interloc_ref(1)
+      if node.stem in ('we', 'us', 'ourselves'):
+        return get_interloc_ref(1, plur=True)
+      if node.stem == 'you':
+        return get_interloc_ref(2, plur=ANY)
+      if node.stem == 'yourself':
+        return get_interloc_ref(2)
+      if node.stem == 'yourselves':
+        return get_interloc_ref(2, plur=True)
+      if node.stem in ('it', 'itself'):
+        return self.get_pronoun_ref(anim=False)
+      if node.stem in ('they', 'them', 'themselves'):
+        return self.get_pronoun_ref(anim=ANY, plur=True)
+    if node.tag == 'DT':
+      if node.stem in ('that', 'this'):
+        return self.get_pronoun_ref(anim=False)
+      if node.stem in ('those', 'these'):
+        return self.get_pronoun_ref(anim=False, plur=True)
     if (node.tag == 'NN' and 'det' not in node.children) or node.tag == 'NNP':
       return FixedRef(node.stem)
-    if node.tag == 'PRP':
-      if node.stem == 'I' or node.stem == 'myself':
-        return get_interloc_ref(1)
-      if node.stem == 'we' or node.stem == 'ourselves':
-        return get_interloc_ref(1, plur=True)
-      if node.stem == 'you' or node.stem == 'yourself':
+    if node.tag == 'NN':
+      pass
     # Indefinites become templates
     # Definites become templates restricted to existing referents
     # Pronouns are dereferenced to their last-mention buddy
